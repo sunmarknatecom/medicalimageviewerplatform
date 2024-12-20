2a1c3118b2e4f980fc7e5eda70282f5b864e0e4b
sunmarknatecom <sunmark@nate.com>
2024-12-20 14:41:57 +0900

    add label

--- medicalimageviwerplatform.py
+++ medicalimageviwerplatform.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
 
 class MedicalImageViewerPlatform(QMainWindow):
     def __init__(self):
@@ -9,7 +10,15 @@ class MedicalImageViewerPlatform(QMainWindow):
     def setup_ui(self):
         self.setWindowTitle("Medical Image Viewer Platform v1.0.0")
         self.setGeometry(300, 300, 400, 400)
-        
+
+        # Create Central Widget
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        # Create Layout
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
         # Create Menubar
         self.menubar = self.menuBar()
         self.menubar.setNativeMenuBar(False)
@@ -22,6 +31,12 @@ class MedicalImageViewerPlatform(QMainWindow):
 
         # Help Menu
         self.create_help_menu()
+
+        # Add Label to Layout
+        self.create_label_CT_button(layout)
+
+        # Status Bar
+        self.statusBar().showMessage("Ready")
 
     def create_file_menu(self):
         file_menu = self.menubar.addMenu("파일")
@@ -42,15 +57,22 @@ class MedicalImageViewerPlatform(QMainWindow):
 
     def create_help_menu(self):
         help_menu = self.menubar.addMenu("도움말")
-
         self.doc_action = QAction("Documentation")
         self.release_action = QAction("Release Notes")
         self.license_action = QAction("View License")
-
         help_menu.addSeparator()
         help_menu.addAction(self.doc_action)
         help_menu.addAction(self.release_action)
         help_menu.addAction(self.license_action)
+        help_menu.addSeparator()
+
+    def create_label_CT_button(self, layout):
+        label_CT_open_button = QLabel("CT folder", self)
+        label_CT_open_button.setAlignment(Qt.AlignCenter)
+        font = label_CT_open_button.font()
+        font.setFamily("Times New Roman")
+        label_CT_open_button.setFont(font)
+        layout.addWidget(label_CT_open_button)
 
 # Main
 if __name__ == "__main__":
